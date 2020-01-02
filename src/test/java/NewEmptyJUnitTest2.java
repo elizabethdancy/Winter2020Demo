da290e7cd93fcfad9e99448a94d89cb9ac425aba
elizabethdancy <elizabeth.dancy@sheridancollege.ca>
2020-01-02 09:50:56 -0500

    Initial commit for midterm exam part C

---
+++ src/test/java/NewEmptyJUnitTest2.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author dancye
+ */
+public class NewEmptyJUnitTest2 {
+    
+    public NewEmptyJUnitTest2() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    @Test
+    public void testNothing()
+    {
+        assertTrue(true);
+    }
+}
