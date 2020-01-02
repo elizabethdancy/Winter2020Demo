983e013a02703205288d04dd719ffaa013cd013b
elizabethdancy <elizabeth.dancy@sheridancollege.ca>
2020-01-02 09:56:43 -0500

    cmmit my new class

--- src/main/java/com/mycompany/testjunit/NewClass.java
+++ src/main/java/com/mycompany/testjunit/NewClass.java
@@ -10,5 +10,9 @@ package com.mycompany.testjunit;
  * @author dancye
  */
 public class NewClass {
+    public static void main(String[] args)
+    {
+        System.out.println("test");
+    }
     
 }
